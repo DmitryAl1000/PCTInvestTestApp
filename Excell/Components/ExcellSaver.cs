d66d0f219454cbe99ef3d0b5ae395876cdd58f1e
DmitryAl1000 <168642362+DmitryAl1000@users.noreply.github.com>
2024-10-09 17:01:29 +0300

    Add files via upload

---
+++ Excell/Components/ExcellSaver.cs
@@ -0,0 +1,237 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lib_Excell
+{
+    public class ExcellSaver
+    {
+        List<string> _log;
+
+        Workbook _excelBook = new Workbook(); // книга
+        Worksheet _sheet;           // страница
+        Cells _cells;
+        Cell _cell;
+
+        string _filePath = string.Empty;
+        int _pageForRecord;
+
+        public ExcellSaver(string filePath, List<string> log, int pageForRecord = 0)
+        {
+            this._filePath = filePath;
+            _log = log;
+            if (pageForRecord < 0)
+                pageForRecord = 0;
+            this._pageForRecord = pageForRecord;
+            _sheet = _excelBook.Worksheets[_pageForRecord];
+            _cells = _sheet.Cells;
+            _cell = _cells["A1"]; 
+        }
+
+        // По номеру создаем название колонки. Напирмер 28 - "AC"
+        private string ColumName(int num)
+        {
+            int abcCount = 26; // символов в латинском алфавите
+            Queue<string> simbol = new Queue<string>();
+            Dictionary<int, string> abc = new Dictionary<int, string>()
+            {
+                { 0,"A" },
+                { 1, "B" },
+                { 2, "C" },
+                { 3, "D" },
+                { 4, "E" },
+                { 5,"F" },
+                { 6,"G" },
+                { 7,"H" },
+                { 8, "I" },
+                { 9,"J" },
+                { 10, "K" },
+                { 11, "L" },
+                { 12, "M" },
+                { 13, "N" },
+                { 14, "O" },
+                { 15, "P" },
+                { 16, "Q" },
+                { 17, "R" },
+                { 18, "S" },
+                { 19, "T" },
+                { 20, "U" },
+                { 21, "V" },
+                { 22, "W" },
+                { 23, "X" },
+                { 24, "Y" },
+                { 25, "Z" },
+            };
+
+            int result = num / abcCount;
+            if (result != 0 )
+                simbol.Enqueue(abc[result-1]);
+            int remains = num % abcCount;
+                simbol.Enqueue(abc[remains]);
+
+            string culumName = string.Empty;
+            foreach (var item in simbol) //складываем стек
+                culumName += item;
+
+            return culumName;
+        }
+
+        public void setPageForRecord(int pageForRecord) => this._pageForRecord = pageForRecord;
+
+        //записать линию. Подойдет для заголовка
+        public void RecordLine(string[] array, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0)
+                startRowY = 0;
+            if (startColumX < 0)
+                startColumX = 0;
+
+            startRowY++; //смещение на 1 тк в экселе строки начинаются с 1, а не с нуля
+
+            for (int x = 0; x < array.Length; x++)
+            {
+                string columName = ColumName(x + startColumX);
+                string cellForRecord = columName + startRowY.ToString(); //y тут всегда одинаковый
+                _cell = _cells[cellForRecord];
+                _cell.PutValue(array[x]); 
+            }
+        }
+
+        //записать колонку
+        public void RecordColum(string[] array, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0)
+                startRowY = 0;
+            if (startColumX < 0)
+                startColumX = 0;
+            int arraySizeY = array.Length;
+
+            string columName = ColumName(startColumX);
+                for (int y = 0 ; y <  arraySizeY ; y++) 
+                {
+                    string cellForRecord = columName + (y + 1 + startRowY).ToString(); //смещение на 1, тк в экселе строки начинаются с 1
+                _cell = _cells[cellForRecord];
+                    _cell.PutValue(array[y]); 
+                }
+        }
+
+        public void RecordColum(List<string> array, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0)
+                startRowY = 0;
+            if (startColumX < 0)
+                startColumX = 0;
+            int arraySizeY = array.Count;
+
+            string columName = ColumName(startColumX);
+            for (int y = 0; y < arraySizeY; y++)
+            {
+                string cellForRecord = columName + (y + 1 + startRowY).ToString(); //смещение на 1, тк в экселе строки начинаются с 1
+                _cell = _cells[cellForRecord];
+                _cell.PutValue(array[y]);
+            }
+        }
+
+        public void Record2DArray(Dictionary<string,string> dictionary, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0) startRowY = 0;
+            if (startColumX < 0) startColumX = 0;
+
+
+            string keyColumName = ColumName(0 + startColumX);
+            string ValueColumName = ColumName(1 + startColumX);
+            int i = 0;
+            foreach (var item in dictionary)
+            {
+                string cellForRecord = keyColumName + (i + 1 + startRowY).ToString(); //смещение на 1, тк в экселе строки начинаются с 1, а не с нуля
+                _cell = _cells[cellForRecord];
+                _cell.PutValue(item.Key);
+
+                cellForRecord = ValueColumName + (i + 1 + startRowY).ToString(); //смещение на 1, тк в экселе строки начинаются с 1, а не с нуля
+                _cell = _cells[cellForRecord];
+                _cell.PutValue(item.Value);
+                i++;
+            }
+        }
+
+        public void Record2DArray(List<string[]> list, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0)
+                startRowY = 0;
+            if (startColumX < 0)
+                startColumX = 0;
+
+            for (int i = startRowY; i < startRowY + list.Count; i++)
+                RecordLine(list[i], i, startColumX);
+        }
+
+        /// <summary>
+        /// записать двумерный массив
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startRowY"></param>
+        /// <param name="startColumX"></param>
+        public void Record2DArray(string[,] array, int startRowY = 0, int startColumX = 0)
+        {
+            if (startRowY < 0)
+                startRowY = 0;
+            if (startColumX < 0)
+                startColumX = 0;
+
+
+            int arraySizeX = array.GetLength(0);
+            int arraySizeY = array.GetLength(1);
+
+            for (int x = 0; x < arraySizeX; x++)
+            {
+                string columName = ColumName(x + startColumX);
+                for (int y = 0 ; y <  arraySizeY ; y++) 
+                {
+                    string cellForRecord = columName + (y + 1 + startRowY ).ToString(); //смещение на 1, тк в экселе строки начинаются с 1, а не с нуля
+                    _cell = _cells[cellForRecord];
+                    _cell.PutValue(array[x, y ]); 
+                }
+            }
+        }
+
+        public void Save()
+        {
+            if (!_filePath.Contains(".xlsx"))
+                _filePath += ".xlsx";
+            try
+            {
+                _excelBook.Save(_filePath, SaveFormat.Xlsx);
+                _log.Add($"файл сохранен {_filePath}");
+            }
+            catch (Exception)
+            {
+                _log.Add($"{_filePath} - Ошибка записи файла, возможно он занят другим приложением");
+            }
+        }
+
+        // методы посредники
+        public void Record2DArrayAndSave(string[,] array, int startRowY = 0, int startColumX = 0)
+        {
+            Record2DArray(array, startRowY, startColumX);
+            Save();
+        }
+
+        // асинхронные
+        public async Task SaveAsync()
+        {
+            await Task.Run(() => Save());
+        }
+
+        public async Task Record2DArrayAndSaveAsync(string[,] array, int startRowY = 0, int startColumX = 0)
+        {
+            await Task.Run(() => Record2DArrayAndSave(array, startRowY, startColumX));
+        }
+
+
+    }
+}
---
+++ Excell/Components/LoadFromExcel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Cells;
+using System.IO;
+
+namespace Excell
+{
+    public class LoadFromExcel
+    {
+        List<string> _log;
+
+        string _filePath;
+        int _pageNumber = 0;
+        WorksheetCollection _collection;
+        Worksheet _workPage;
+        int _maxColumX;
+        int _maxRowsY;
+
+        string[,] _arrayExcell;
+
+        public LoadFromExcel(string filePath, List<string> log, int pageNumber = 0)
+        {
+            this._filePath = filePath ;
+            this._pageNumber = pageNumber;
+            this._log = log ;
+
+
+            if (CreateCollectionOfPage())
+            {
+                SetPageNumber(_pageNumber);
+                CrateArray();
+            }
+        }
+
+        public string[,] GetArray()
+        {
+            return _arrayExcell;
+        }
+
+
+        // =========== 1. подгрузка и создание массива из файла .xlsx =============
+        private bool CreateCollectionOfPage()
+        {
+            if (!File.Exists(_filePath))
+            {
+                _log.Add($"Файл не существует {_filePath}");
+                return false;
+            }
+            else
+            {
+                _log.Add($"Файл загружен {_filePath}");
+                Workbook wb = new Workbook(_filePath);
+                _collection = wb.Worksheets;
+                return true;
+            }
+        }
+
+        private void SetPageNumber(int pageNumber)
+        {
+            _pageNumber = pageNumber;
+            _workPage = _collection[_pageNumber]; // установка нужно листа
+        }
+        private void SetMaxDataCoulumX() => _maxColumX = _workPage.Cells.MaxDataColumn + 1; //+1 потому что ячейки не с нуля нумеруются
+        private void SetMaxDataRowY() => _maxRowsY = _workPage.Cells.MaxDataRow + 1;
+        private void CrateArray()
+        {
+            SetMaxDataCoulumX();
+            SetMaxDataRowY();
+            string[,] array = new string[_maxColumX, _maxRowsY];
+            for (int columX = 0; columX < _maxColumX; columX++)
+            {
+                for (int rowsY = 0; rowsY < _maxRowsY; rowsY++)
+                    array[columX, rowsY] = _workPage.Cells[rowsY, columX].Value?.ToString() ?? string.Empty;
+            }
+            _arrayExcell = array;
+        }
+
+
+        //делает из первых двух столбцов словарь, остальные столбцы игнорирует
+        public Dictionary<string, string> GetDictionary()
+        {
+            Dictionary<string, string> dictionary = new();
+            for (int x = 0; x < _arrayExcell.GetLength(1); x++)
+            {
+                if (!dictionary.ContainsKey(_arrayExcell[0, x]))
+                    dictionary.Add(_arrayExcell[0, x], _arrayExcell[1, x]);
+            }
+
+            return dictionary;
+        }
+
+
+    }
+}
